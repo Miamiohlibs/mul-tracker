565dce7c898a231d9c581e473f91ae6b78172dcc
Ken Irwin <irwinkr@miamioh.edu>
2020-07-23 13:06:20 -0400

    basic overlap calculation

--- admin/index.php
+++ admin/index.php
@@ -22,22 +22,44 @@ include('../bootstrap.php');
 
 </nav>
 <div class="container">
-<form>
+<form method="POST">
+<input type="hidden" name="formname" value="adminOverlap">
 <label for="username">View overlap with user</label> <?php print(SelectUser($_SESSION['username']));?>
 <input type="submit" class="btn btn-primary btn-sm py-1" />
 </form>
 </div>
 
+<div class="main container">
+    <?php
+    if ($_REQUEST['formname'] == 'adminOverlap') {
+        GetOverlap($_SESSION['username']);
+    }
+    ?>
+</div>
+
+
+
 <?php
-function GetOverlap($user, $start, $end) {
+function GetOverlap($user, $start=null, $end=null) {
 // https://stackoverflow.com/questions/6571538/checking-a-table-for-time-overlap
-$q = "SELECT *
+    print '<h2>Getting building overlaps for: '.$_SESSION['display_name'].'</h2>'.PHP_EOL;
+$q ="SELECT a.username as subject_name, a.time_in as subject_in, a.time_out as subject_out, a.building as building, b.username as cmp_name, b.time_in as cmp_in, b.time_out as subj_out
 FROM sessions  a
 JOIN sessions b on a.time_in <= b.time_out
     and a.time_out >= b.time_in
     and a.username != b.username
-    and a.building = b.building
     WHERE a.username = ?";
+
+    print '<div class="alert alert-info">'.$q.'</div>'.PHP_EOL;
+    
+    global $pdo;
+    $stmt = $pdo->prepare($q); 
+    $stmt->bindValue(1, $_SESSION['username']);
+    $stmt->execute();
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    print '<pre>';
+    print_r($rows);
+    print '</pre>';
 }
 
 ?>
