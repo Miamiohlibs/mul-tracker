0dbb8b584f38d56e316bf1c637acdf2945533150
Ken Irwin <irwinkr@miamioh.edu>
2020-07-28 14:57:16 -0400

    prettier table for who is in building

--- admin/index.php
+++ admin/index.php
@@ -98,13 +98,26 @@ elseif ($_REQUEST['formname'] == 'GetNow') {
     print ('</pre>');
 */
     $currBuilding = '';
+    $bldgPeople = array();
+
     foreach ($rows as $row) {
         if ($row['building'] != $currBuilding) { 
-            print '<h2>'.$row['building'].'</h2>';
             $currBuilding = $row['building'];
+            $bldgPeople[$currBuilding] = '';
         }
-        print '<li>'.$row['name'].'</li>';
+//        $durationTs = time() - strtotime($row['time_in']);
+        $duration = get_time_ago(strtotime($row['time_in']));
+        $bldgPeople[$currBuilding] .= '<tr><td>'.$row['name'].'</td><td>'.$row['time_in'].'</td><td>'.$duration.'</td></tr>';
     }
+
+    foreach ($bldgPeople as $bldg => $people) {
+        print '<table class="table mb-5 mt-3 w-100">';
+        print '<thead><tr class="thead-dark"><th colspan="3" class="h4">'.$bldg.'</th></tr>';
+        print '<tr><th width="25%">Name</th><th width="50%">Time In</th><th width="25%">Duration</th></thead><tbody>';
+        print $people;
+        print '</tbody></table>';
+    }
+
 }
     ?>
 </div>
@@ -179,4 +192,86 @@ function GetNames() {
     }
     return $arr;
 }
+
+function get_time_ago($time_stamp)
+//https://stackoverflow.com/questions/2915864/php-how-to-find-the-time-elapsed-since-a-date-time
+{
+    $time_difference = strtotime('now') - $time_stamp;
+
+    if ($time_difference >= 60 * 60 * 24 * 365.242199)
+    {
+        /*
+         * 60 seconds/minute * 60 minutes/hour * 24 hours/day * 365.242199 days/year
+         * This means that the time difference is 1 year or more
+         */
+        return get_time_ago_string($time_stamp, 60 * 60 * 24 * 365.242199, 'year');
+    }
+    elseif ($time_difference >= 60 * 60 * 24 * 30.4368499)
+    {
+        /*
+         * 60 seconds/minute * 60 minutes/hour * 24 hours/day * 30.4368499 days/month
+         * This means that the time difference is 1 month or more
+         */
+        return get_time_ago_string($time_stamp, 60 * 60 * 24 * 30.4368499, 'month');
+    }
+    elseif ($time_difference >= 60 * 60 * 24 * 7)
+    {
+        /*
+         * 60 seconds/minute * 60 minutes/hour * 24 hours/day * 7 days/week
+         * This means that the time difference is 1 week or more
+         */
+        return get_time_ago_string($time_stamp, 60 * 60 * 24 * 7, 'week');
+    }
+    elseif ($time_difference >= 60 * 60 * 24)
+    {
+        /*
+         * 60 seconds/minute * 60 minutes/hour * 24 hours/day
+         * This means that the time difference is 1 day or more
+         */
+        return get_time_ago_string($time_stamp, 60 * 60 * 24, 'day');
+    }
+    elseif ($time_difference >= 60 * 60)
+    {
+        /*
+         * 60 seconds/minute * 60 minutes/hour
+         * This means that the time difference is 1 hour or more
+         */
+        return get_time_ago_string($time_stamp, 60 * 60, 'hour');
+    }
+    else
+    {
+        /*
+         * 60 seconds/minute
+         * This means that the time difference is a matter of minutes
+         */
+        return get_time_ago_string($time_stamp, 60, 'minute');
+    }
+}
+
+function get_time_ago_string($time_stamp, $divisor, $time_unit)
+{
+    $time_difference = strtotime("now") - $time_stamp;
+    $time_units      = floor($time_difference / $divisor);
+
+    settype($time_units, 'string');
+
+    if ($time_units === '0')
+    {
+        return 'less than 1 ' . $time_unit . ' ago';
+    }
+    elseif ($time_units === '1')
+    {
+        return '1 ' . $time_unit . ' ago';
+    }
+    else
+    {
+        /*
+         * More than "1" $time_unit. This is the "plural" message.
+         */
+        // TODO: This pluralizes the time unit, which is done by adding "s" at the end; this will not work for i18n!
+        return $time_units . ' ' . $time_unit . 's ago';
+    }
+}
+
+
 ?>
